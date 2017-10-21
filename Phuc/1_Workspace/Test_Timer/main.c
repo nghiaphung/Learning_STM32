8db96d5a3a207dbffe9193929724a218aa57cefb
Achilles69 <nvphuc69@gmail.com>
2017-10-21 18:20:22 +0700

    Them_ClockCmd_Cho_Timer

--- Phuc/1_Workspace/Test_Timer/main.c
+++ Phuc/1_Workspace/Test_Timer/main.c
@@ -4,9 +4,9 @@
 #include "stm32f10x_tim.h"
 
 void LED (void);
-RCC_ClocksTypeDef RCC_ClockStatus; 							// RCC_ClocksStatus: pointer to an RCC_ClocksTypeDef structure //
-TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;	
-NVIC_InitTypeDef NVIC_InitStruct;
+
+	
+
 void KHOI_TAO_GPIO (void);
 void KHOI_TAO_TIMER (void);
 void Blink_Led (void);
@@ -30,7 +30,7 @@ int main (void)
 		GPIOC_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;			// Khai bao o che do Output Push-Pull
 		GPIO_Init(GPIOC,&GPIOC_InitStructure);								// Khoi tao GPIOC theo nhu da khai bao 
 	
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // Khoi dong clock cho Port C
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA , ENABLE); // Khoi dong clock cho Port C
 		GPIO_InitTypeDef GPIOA_InitStructure;									// Khoi tao khai bao GPIO
 		GPIOA_InitStructure.GPIO_Pin = GPIO_Pin_0;						// Khai bao la chan so 0
 		GPIOA_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		// Khai bao toc do la 50Mhz
@@ -42,13 +42,16 @@ int main (void)
 
 	void KHOI_TAO_TIMER (void)
 	{
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
+		RCC_ClocksTypeDef RCC_ClockStatus; 							// RCC_ClocksStatus: pointer to an RCC_ClocksTypeDef structure //
 		// Get clock status //
 		RCC_GetClocksFreq(&RCC_ClockStatus);												
 		// Gets PCLK1_Frequency //
 		u16 PCLK1_Frequency = RCC_ClockStatus.PCLK1_Frequency; 			
 		// Calculate pre-scaler //
 		u16 TIM1_Prescaler = ((PCLK1_Frequency/1000000)-1);							  //(TIM1_Prescaler) CLK on 1 us //
-		// setup timer's param //				
+		// setup timer's param //
+		TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;		
 		TIM_TimeBaseInitStruct.TIM_Period = (1000-1);
 		TIM_TimeBaseInitStruct.TIM_Prescaler = TIM1_Prescaler;
 		TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
@@ -57,6 +60,7 @@ int main (void)
 		TIM_TimeBaseInit(TIM1,&TIM_TimeBaseInitStruct);
 		TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
 		//  initialize NVIC //
+		NVIC_InitTypeDef NVIC_InitStruct;
 		NVIC_InitStruct.NVIC_IRQChannel = TIM1_CC_IRQn;
 		NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 		NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 2;
